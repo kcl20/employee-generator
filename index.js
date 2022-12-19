3542890ba7ffee6a9c0c37591953d9e21ef4b7e6
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-12-19 13:54:07 -0500

    Got basic structure of index.html to generate.

--- index.js
+++ index.js
@@ -88,7 +88,9 @@ function EngineerOrIntern () {
         return;
     } else if (data.nextAction == "Neither, I'm done.") {
       // printHTML();
-      console.log("Completed, generating HTML.")}
+      console.log("Completed, generating HTML.")
+      generateHTML();
+    }
     })
   };
 
@@ -151,5 +153,61 @@ function addIntern () {
       EngineerOrIntern();
     });
 }
+
+function generateHTML() {
+
+  fs.writeFile("index.html", employeeProfilePageContent, (err) =>
+  err ? console.error(err) : console.log('Success!')
+  );
+  
+
+}
+
+var employeeProfilePageContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <header>
+        <h1>Team Profile Generator</h1>
+    </header>
+    <main>
+        <div class="manager">
+            <h2>${manager.name}</h2>
+            <h3>Manager</h3>
+            <p>Employee ID: ${manager.employeeID}</p>
+            <p>Email: ${manager.email}</p>
+            <p>Office Number: ${manager.officeNumber}</p>
+        </div>
+        
+        <div class="engineer">`
+        
+        for (var i = 0; i < engineers.length; i++) {
+            employeeProfilePageContent += `
+            <h2>${engineers[i].name}</h2>
+            <h3>Engineer</h3>
+            <p>Employee ID: ${engineers[i].employeeID}</p>
+            <p>Email: ${engineers[i].email}</p>
+            <p>GitHub: ${engineers[i].github}</p>`}
+      `</div>
+      
+      <div class="intern">`
+      
+       for (var i = 0; i < interns.length; i++) {
+        employeeProfilePageContent += `
+        <h2>${interns[i].name}</h2>
+        <h3>Intern</h3>
+        <p>Employee ID: ${interns[i].employeeID}</p>
+        <p>Email: ${interns[i].email}</p>
+        <p>School: ${interns[i].school}</p>`}
+        `</div>
+    </main>
+    </body>
+    </html>`
+
 
 init();
